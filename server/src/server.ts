fca5332e145636ced3d24ab38dfdb94e07a53817
cooolbros <pfwobcke@gmail.com>
2021-08-19 20:59:29 +1000

    Add highlighting + Remove code

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,9 @@
-import { TextDocument } from 'vscode-languageserver-textdocument';
+import { TextDocument } from "vscode-languageserver-textdocument";
 import {
-	CompletionItem,
-	CompletionItemKind, createConnection, Diagnostic, DiagnosticSeverity, InitializeParams,
+	createConnection, Diagnostic, DiagnosticSeverity, InitializeParams,
 	InitializeResult,
 	Position,
-	ProposedFeatures, Range, TextDocumentChangeEvent, TextDocumentPositionParams,
-	TextDocuments,
+	ProposedFeatures, Range, TextDocumentChangeEvent, TextDocuments,
 	TextDocumentSyncKind, _Connection
 } from "vscode-languageserver/node";
 import { VDF } from "./vdf";
@@ -13,11 +11,8 @@ import { VDF } from "./vdf";
 const connection: _Connection = createConnection(ProposedFeatures.all)
 const documents: TextDocuments<TextDocument> = new TextDocuments(TextDocument)
 
-let hasConfigurationCapability = false;
-let hasWorkspaceFolderCapability = false;
-let hasDiagnosticRelatedInformationCapability = false;
-
 connection.onInitialize((params: InitializeParams): InitializeResult => {
+	connection.console.log("connection.onInitialize")
 	return {
 		capabilities: {
 			textDocumentSync: TextDocumentSyncKind.Incremental
@@ -44,119 +39,10 @@ documents.onDidChangeContent((change: TextDocumentChangeEvent<TextDocument>): vo
 						)
 					}
 				]
-				// if (e instanceof VDFError) {
-				// 	return [
-				// 		{
-				// 			range: {
-				// 				start: change.document.positionAt(e.position),
-				// 				end: change.document.positionAt(e.position)
-				// 			},
-				// 			severity: DiagnosticSeverity.Error,
-				// 			message: e.message
-				// 		}
-				// 	]
-				// }
 			}
 		})()
 	})
 })
-
-
-// The example settings
-interface ExampleSettings {
-	maxNumberOfProblems: number;
-}
-
-// The global settings, used when the `workspace/configuration` request is not supported by the client.
-// Please note that this is not the case when using this server with the client provided in this example
-// but could happen with other clients.
-const defaultSettings: ExampleSettings = { maxNumberOfProblems: 1000 };
-let globalSettings: ExampleSettings = defaultSettings;
-
-// Cache the settings of all open documents
-const documentSettings: Map<string, Thenable<ExampleSettings>> = new Map();
-
-connection.onDidChangeConfiguration(change => {
-	if (hasConfigurationCapability) {
-		// Reset all cached document settings
-		documentSettings.clear();
-	} else {
-		globalSettings = <ExampleSettings>(
-			(change.settings.languageServerExample || defaultSettings)
-		);
-	}
-
-	// Revalidate all open text documents
-	// documents.all().forEach(validateTextDocument);
-});
-
-function getDocumentSettings(resource: string): Thenable<ExampleSettings> {
-	if (!hasConfigurationCapability) {
-		return Promise.resolve(globalSettings);
-	}
-	let result = documentSettings.get(resource);
-	if (!result) {
-		result = connection.workspace.getConfiguration({
-			scopeUri: resource,
-			section: 'languageServerExample'
-		});
-		documentSettings.set(resource, result);
-	}
-	return result;
-}
-
-// Only keep settings for open documents
-documents.onDidClose(e => {
-	documentSettings.delete(e.document.uri);
-});
-
-// The content of a text document has changed. This event is emitted
-// when the text document first opened or when its content has changed.
-// documents.onDidChangeContent(change => {
-// 	// validateTextDocument(change.document);
-// });
-
-connection.onDidChangeWatchedFiles(_change => {
-	// Monitored files have change in VSCode
-	connection.console.log('We received an file change event');
-});
-
-// This handler provides the initial list of the completion items.
-connection.onCompletion(
-	(_textDocumentPosition: TextDocumentPositionParams): CompletionItem[] => {
-		// The pass parameter contains the position of the text document in
-		// which code complete got requested. For the example we ignore this
-		// info and always provide the same completion items.
-		return [
-			{
-				label: 'TypeScript',
-				kind: CompletionItemKind.Text,
-				data: 1
-			},
-			{
-				label: 'JavaScript',
-				kind: CompletionItemKind.Text,
-				data: 2
-			}
-		];
-	}
-);
-
-// This handler resolves additional information for the item selected in
-// the completion list.
-connection.onCompletionResolve(
-	(item: CompletionItem): CompletionItem => {
-		if (item.data === 1) {
-			item.detail = 'TypeScript details';
-			item.documentation = 'TypeScript documentation';
-		} else if (item.data === 2) {
-			item.detail = 'JavaScript details';
-			item.documentation = 'JavaScript documentation';
-		}
-		return item;
-	}
-);
-
 
 documents.listen(connection)
 connection.listen()
