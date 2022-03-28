65fbf2dc978b5eeeb7a056ff0e6d68e33fe6aa45
cooolbros <pfwobcke@gmail.com>
2022-03-28 21:03:26 +1100

    Generate Popfile syntax highlighting using Language Server autocompletion values + Add values from syntax highlighting to Popfile Language Server autocompletion

--- syntaxes/generate_popfile.tmLanguage.py
+++ syntaxes/generate_popfile.tmLanguage.py
@@ -1,5 +1,13 @@
-import os
+import functools
 import json
+
+JSON_AUTOCOMPLETION_PATH = "../servers/popfile/src/JSON/autocompletion"
+KEYS = json.load(open(f"{JSON_AUTOCOMPLETION_PATH}/keys.json"))
+VALUES = json.load(open(f"{JSON_AUTOCOMPLETION_PATH}/values.json"))
+
+entity_name_type_enum = sorted(set(map(lambda i: i.lower(), VALUES.keys())))
+keyword = sorted(functools.reduce(lambda a, b: a.union(list([b[0].lower()]) + list(map(lambda i: (i[1:] if i.startswith("~") else i).lower(), b[1]))), KEYS.items(), set()))[2:]
+variable_other_enummember = sorted(list(functools.reduce(lambda a, b: a.union(b), map(lambda i: map(lambda i: i.lower(), i), VALUES.values()), set())) + list(["spawnbot.*"]))
 
 print(json.dumps({
     "$schema": "https://raw.githubusercontent.com/martinring/tmlanguage/master/tmlanguage.json",
@@ -28,7 +36,22 @@ print(json.dumps({
             "name": "string",
             "match": "\".*\""
         },
-        *list(map(lambda f: { "name": os.path.splitext(f)[0], "match": "(?i)\\b(" + "|".join(map(lambda f: f.strip(), sorted(open(f, "r").readlines()))) + ")\\b" }, filter(lambda f: f.endswith(".txt"), os.listdir()))),
+        {
+            "name": "keyword.control",
+            "match": "(?i)\\b(Action|Target)\\b"
+        },
+        {
+            "name": "entity.name.type.enum",
+            "match": f"(?i)\\b({'|'.join(entity_name_type_enum)})\\b"
+        },
+        {
+            "name": "keyword",
+            "match": f"(?i)\\b({'|'.join(keyword)})\\b"
+        },
+        {
+            "name": "variable.other.enummember",
+            "match": f"(?i)\\b({'|'.join(variable_other_enummember)})\\b"
+        },
         {
             "name": "constant.numeric",
             "match": "\\b(\\d+)\\b"
