e40ca6ad11ac7e65e13cb8aa3c34dc465ea69e69
cooolbros <pfwobcke@gmail.com>
2023-04-15 14:58:15 +1000

    Update extension client

--- extension/desktop/client/src/extension.ts
+++ extension/desktop/client/src/extension.ts
@@ -1,14 +1,20 @@
+import "$lib/client/HUDAnimationsDecoration"
+import { initHUDAnimationsLanguageClientDecorations } from "$lib/client/HUDAnimationsDecoration"
+import { initLanguageClientFileSystem } from "$lib/client/LanguageClientFileSystem"
+import { initLanguageClientRequests } from "$lib/client/LanguageClientRequests"
+import { VSCodeLanguageClientFileSystem } from "$lib/client/VSCodeLanguageClientFileSystem"
 import { copyKeyValuePath } from "$lib/commands/copyKeyValuePath"
 import { extractVPKFileToWorkspace } from "$lib/commands/extractVPKFileToWorkspace"
-import { importPopfileTemplates } from "$lib/commands/importPopfileTemplates"
 import { JSONToVDF } from "$lib/commands/JSONToVDF"
 import { showReferences } from "$lib/commands/showReferences"
 import { VDFToJSON } from "$lib/commands/VDFToJSON"
 import { languageClientsInfo } from "$lib/languageClientsInfo"
+import { VSCodeVDFLanguageIDSchema } from "$lib/types/VSCodeVDFLanguageID"
+import { VPKFileSystemProvider } from "$lib/VPK/VPKFileSystemProvider"
+import { VPKManager } from "$lib/VPK/VPKManager"
 import { join } from "path"
 import { commands, ExtensionContext, TextDocument, window, workspace } from "vscode"
 import { LanguageClient, LanguageClientOptions, ServerOptions, TransportKind } from "vscode-languageclient/node"
-import { VPKTextDocumentContentProvider } from "./VPKTextDocumentContentProvider"
 import { VTFEditor } from "./VTF/VTFEditor"
 
 const languageClients: { -readonly [P in keyof typeof languageClientsInfo]?: LanguageClient } = {}
@@ -16,70 +22,102 @@ const languageClients: { -readonly [P in keyof typeof languageClientsInfo]?: Lan
 export function activate(context: ExtensionContext): void {
 
 	// https://code.visualstudio.com/api/references/contribution-points#contributes
+	// https://code.visualstudio.com/api/references/vscode-api
 
 	// Commands
 
 	context.subscriptions.push(commands.registerTextEditorCommand("vscode-vdf.copyKeyValuePath", copyKeyValuePath))
 	context.subscriptions.push(commands.registerTextEditorCommand("vscode-vdf.extractVPKFileToWorkspace", extractVPKFileToWorkspace))
-	context.subscriptions.push(commands.registerTextEditorCommand("vscode-vdf.importPopfileTemplates", importPopfileTemplates))
 	context.subscriptions.push(commands.registerTextEditorCommand("vscode-vdf.JSONToVDF", JSONToVDF))
 	context.subscriptions.push(commands.registerTextEditorCommand("vscode-vdf.showReferences", showReferences))
 	context.subscriptions.push(commands.registerTextEditorCommand("vscode-vdf.VDFToJSON", VDFToJSON))
 
-	// VPK Protocol
+	// Window
+	context.subscriptions.push(window.registerCustomEditorProvider("vscode-vdf.VTFEditor", new VTFEditor(context)))
 
-	context.subscriptions.push(workspace.registerTextDocumentContentProvider("vpk", new VPKTextDocumentContentProvider(workspace)))
+	// Workspace
+	const vpks = new VPKManager(new VSCodeLanguageClientFileSystem())
+	context.subscriptions.push(workspace.registerFileSystemProvider("vpk", new VPKFileSystemProvider(vpks), { isCaseSensitive: false, isReadonly: true }))
 
 	// Language Server
 
-	const onDidOpenTextDocument = (e: TextDocument): void => {
+	const onDidOpenTextDocument = async (e: TextDocument): Promise<void> => {
+
 		const languageId: string = e.languageId
 		if (((languageId): languageId is keyof typeof languageClientsInfo => languageId in languageClientsInfo)(languageId)) {
-			if (!languageClients[languageId]) {
+			startServer(languageId)
+		}
+	}
 
-				const serverModule = context.asAbsolutePath(join("dist/desktop/servers", `${languageId}.js`,))
+	const startServer = async (languageId: keyof typeof languageClientsInfo): Promise<void> => {
 
-				const serverOptions: ServerOptions = {
-					run: {
-						module: serverModule,
-						transport: TransportKind.ipc
-					},
-					debug: {
-						module: serverModule,
-						transport: TransportKind.ipc,
-						options: {
-							execArgv: [
-								"--nolazy",
-								"--inspect=6009"
-							]
-						}
-					}
-				}
+		if (languageClients[languageId]) {
+			return
+		}
 
-				const clientOptions: LanguageClientOptions = {
-					documentSelector: [
-						languageId
+		const serverModule = context.asAbsolutePath(join("dist/desktop/servers", `${languageId}.js`))
+
+		const serverOptions: ServerOptions = {
+			run: {
+				module: serverModule,
+				transport: TransportKind.ipc,
+				options: {
+					execArgv: [
+						"--nolazy",
+						"--inspect=6009"
 					]
 				}
+			},
+			debug: {
+				module: serverModule,
+				transport: TransportKind.ipc,
+				options: {
+					execArgv: [
+						"--nolazy",
+						"--inspect=6009"
+					]
+				}
+			}
+		}
 
-				languageClients[languageId] = new LanguageClient(
-					languageClientsInfo[languageId].id,
-					languageClientsInfo[languageId].name,
-					serverOptions,
-					clientOptions
-				)
+		const clientOptions: LanguageClientOptions = {
+			documentSelector: [
+				languageId
+			]
+		}
 
-				languageClients[languageId]!.start()
+		const languageClient = languageClients[languageId] = new LanguageClient(
+			`${languageClientsInfo[languageId].id}-language-server`,
+			`${languageClientsInfo[languageId].name} Language Server`,
+			serverOptions,
+			clientOptions
+		)
+
+		initLanguageClientFileSystem(languageClient)
+		context.subscriptions.push(initLanguageClientRequests(languageClients, languageClient))
+
+		// Decorations
+		if (languageId == "hudanimations") {
+			context.subscriptions.push(initHUDAnimationsLanguageClientDecorations(languageClient))
+		}
+
+		await languageClient.start()
+		const servers = VSCodeVDFLanguageIDSchema.array().optional().parse(languageClient.initializeResult?.["servers"])
+
+		if (servers) {
+			for (const languageId of servers) {
+				try {
+					startServer(languageId)
+				}
+				catch (error: any) {
+					window.showErrorMessage(error.message)
+				}
 			}
 		}
 	}
 
 	workspace.textDocuments.forEach(onDidOpenTextDocument)
 	workspace.onDidOpenTextDocument(onDidOpenTextDocument)
-
-	// Custom Editors
-
-	context.subscriptions.push(window.registerCustomEditorProvider("vscode-vdf.VTFEditor", new VTFEditor(context)))
 }
 
 export async function deactivate(): Promise<void[]> {
