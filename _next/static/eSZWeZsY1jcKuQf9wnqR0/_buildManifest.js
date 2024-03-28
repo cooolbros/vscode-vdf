cc478c442e260038e69f29006c34b72012db2011
cooolbros <cooolbros@users.noreply.github.com>
2024-03-28 03:07:34 +0000

    deploy: cd05605888aa8a80344c631f86e9249671115208

--- _next/static/eSZWeZsY1jcKuQf9wnqR0/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/pages/index-ef08866d17d2e57e.js"],"/_error":["static/chunks/pages/_error-38589ec26297365a.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
