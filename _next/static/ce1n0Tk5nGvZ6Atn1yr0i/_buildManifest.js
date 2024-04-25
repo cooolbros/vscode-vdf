7fdd27116284a6a284a57d066441b244adf180fb
cooolbros <cooolbros@users.noreply.github.com>
2024-04-25 15:25:38 +0000

    deploy: aa8844f7532987893fdd26497756d0360085b499

---
+++ _next/static/ce1n0Tk5nGvZ6Atn1yr0i/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/pages/index-0fcc9744761c51fa.js"],"/_error":["static/chunks/pages/_error-1ac136249be010d2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- _next/static/uMbNCtO6R-VDROckEl0eK/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/pages/index-156a2cef223a6c01.js"],"/_error":["static/chunks/pages/_error-1ac136249be010d2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
