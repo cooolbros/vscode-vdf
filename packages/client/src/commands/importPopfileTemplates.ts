e86f8f9903a467a9abb4f1ac618703a8b8d5e506
cooolbros <pfwobcke@gmail.com>
2024-08-01 12:11:58 +1000

    Fix importPopfileTemplates

--- packages/client/src/commands/importPopfileTemplates.ts
+++ packages/client/src/commands/importPopfileTemplates.ts
@@ -1,14 +1,13 @@
-import { existsSync, readFileSync } from "fs"
 import path, { dirname } from "path"
-import { fileURLToPath } from "url"
 import { VDFPosition } from "vdf"
-import { VDFDocumentSymbol, VDFDocumentSymbols, getVDFDocumentSymbols } from "vdf-documentsymbols"
-import { EndOfLine, Position, Uri, type TextEditor, type TextEditorEdit } from "vscode"
+import { getVDFDocumentSymbols, VDFDocumentSymbol, VDFDocumentSymbols } from "vdf-documentsymbols"
+import { EndOfLine, Position, Uri, workspace, WorkspaceEdit, type TextEditor } from "vscode"
 import { TextDocument } from "vscode-languageserver-textdocument"
+import { trpc } from "../TRPCClient"
 
 const TFBotSquadRandomChoice = ["TFBot", "Squad", "RandomChoice"].map(i => i.toLowerCase())
 
-export function importPopfileTemplates(editor: TextEditor, edit: TextEditorEdit): void {
+export async function importPopfileTemplates(editor: TextEditor): Promise<void> {
 	const documentSymbols = getVDFDocumentSymbols(editor.document.getText())
 
 	type VDFDocumentSymbolChildren = VDFDocumentSymbol & { children: VDFDocumentSymbols }
@@ -21,18 +20,18 @@ export function importPopfileTemplates(editor: TextEditor, edit: TextEditorEdit)
 		.flatMap((documentSymbol) => documentSymbol.children)
 
 	const templatesInFile = waveSchedule
-		.filter((documentSymbol): documentSymbol is VDFDocumentSymbolChildren => documentSymbol.name.toLowerCase() == "templates" && VDFDocumentSymbolHasChildren(documentSymbol))
+		.filter((documentSymbol): documentSymbol is VDFDocumentSymbolChildren => documentSymbol.key.toLowerCase() == "templates" && VDFDocumentSymbolHasChildren(documentSymbol))
 		.flatMap(documentSymbol => documentSymbol.children)
-		.map((documentSymbol) => documentSymbol.name.toLowerCase())
+		.map((documentSymbol) => documentSymbol.key.toLowerCase())
 
 	const referencedTemplates = waveSchedule
-		.filter((documentSymbol): documentSymbol is VDFDocumentSymbolChildren => documentSymbol.name.toLowerCase() == "wave" && VDFDocumentSymbolHasChildren(documentSymbol))
+		.filter((documentSymbol): documentSymbol is VDFDocumentSymbolChildren => documentSymbol.key.toLowerCase() == "wave" && VDFDocumentSymbolHasChildren(documentSymbol))
 		.flatMap((documentSymbol) => documentSymbol.children)
-		.filter((documentSymbol): documentSymbol is VDFDocumentSymbolChildren => documentSymbol.name.toLowerCase() == "wavespawn" && VDFDocumentSymbolHasChildren(documentSymbol))
+		.filter((documentSymbol): documentSymbol is VDFDocumentSymbolChildren => documentSymbol.key.toLowerCase() == "wavespawn" && VDFDocumentSymbolHasChildren(documentSymbol))
 		.flatMap((documentSymbol) => documentSymbol.children)
-		.filter((documentSymbol): documentSymbol is VDFDocumentSymbolChildren => TFBotSquadRandomChoice.includes(documentSymbol.name.toLowerCase()) && VDFDocumentSymbolHasChildren(documentSymbol))
-		.flatMap((documentSymbol) => documentSymbol.name.toLowerCase() == "tfbot" ? documentSymbol.children : documentSymbol.children.flatMap(documentSymbol => documentSymbol.children ?? []))
-		.filter((documentSymbol): documentSymbol is VDFDocumentSymbolDetail => documentSymbol.name.toLowerCase() == "template" && VDFDocumentSymbolHasDetail(documentSymbol))
+		.filter((documentSymbol): documentSymbol is VDFDocumentSymbolChildren => TFBotSquadRandomChoice.includes(documentSymbol.key.toLowerCase()) && VDFDocumentSymbolHasChildren(documentSymbol))
+		.flatMap((documentSymbol) => documentSymbol.key.toLowerCase() == "tfbot" ? documentSymbol.children : documentSymbol.children.flatMap(documentSymbol => documentSymbol.children ?? []))
+		.filter((documentSymbol): documentSymbol is VDFDocumentSymbolDetail => documentSymbol.key.toLowerCase() == "template" && VDFDocumentSymbolHasDetail(documentSymbol))
 		.map((documentSymbol) => documentSymbol.detail.toLowerCase())
 
 	const referencedTemplatesNotInFile = referencedTemplates.filter(template => !templatesInFile.includes(template))
@@ -42,28 +41,31 @@ export function importPopfileTemplates(editor: TextEditor, edit: TextEditorEdit)
 	}
 
 	const baseFiles = documentSymbols
-		.filter((documentSymbol): documentSymbol is VDFDocumentSymbolDetail => documentSymbol.name == "#base" && VDFDocumentSymbolHasDetail(documentSymbol))
+		.filter((documentSymbol): documentSymbol is VDFDocumentSymbolDetail => documentSymbol.key == "#base" && VDFDocumentSymbolHasDetail(documentSymbol))
 		.map((documentSymbol) => documentSymbol.detail)
 
 	const externalTemplates: [VDFDocumentSymbol, string][] = []
 
 	for (const baseFile of baseFiles) {
-		const baseFileUri = Uri.parse(path.join(dirname(editor.document.uri.toString(true)), baseFile))
-		const baseFilePath = fileURLToPath(baseFileUri.toString(true))
-		if (existsSync(baseFilePath)) {
-			const baseFileContent = readFileSync(baseFilePath, "utf-8")
-			const baseFileTextDocument = TextDocument.create(baseFileUri.toString(true), "popfile", 1, baseFileContent)
+
+		const baseFileUri = ["robot_standard.pop", "robot_giant.pop", "robot_gatebot.pop"].includes(baseFile)
+			? `vpk:///scripts/population/${baseFile}?vpk=misc`
+			: Uri.parse(path.join(dirname(editor.document.uri.toString(true)), baseFile))
+
+		if (await trpc.fileSystem.exists({ uri: baseFileUri.toString(true) })) {
+			const baseFileContent = await trpc.fileSystem.readFile({ uri: baseFileUri.toString(true) })
+			const baseFileTextDocument = TextDocument.create(baseFileUri.toString(false), "popfile", 1, baseFileContent)
 			const baseFileDocumentSymbols = getVDFDocumentSymbols(baseFileContent)
 			const baseFileTemplatesDocumentSymbols = baseFileDocumentSymbols
 				.filter(VDFDocumentSymbolHasChildren)
 				.flatMap((documentSymbol) => documentSymbol.children)
-				.filter((documentSymbol): documentSymbol is VDFDocumentSymbolChildren => documentSymbol.name.toLowerCase() == "templates" && VDFDocumentSymbolHasChildren(documentSymbol))
+				.filter((documentSymbol): documentSymbol is VDFDocumentSymbolChildren => documentSymbol.key.toLowerCase() == "templates" && VDFDocumentSymbolHasChildren(documentSymbol))
 				.flatMap(documentSymbol => documentSymbol.children)
 
 			for (const baseFileTemplateDocumentSymbol of baseFileTemplatesDocumentSymbols) {
-				const templateName = baseFileTemplateDocumentSymbol.name.toLowerCase()
+				const templateName = baseFileTemplateDocumentSymbol.key.toLowerCase()
 				if (referencedTemplatesNotInFile.includes(templateName)) {
-					const existingTemplateDocumentSymbol = externalTemplates.find(template => template[0].name == templateName)
+					const existingTemplateDocumentSymbol = externalTemplates.find(template => template[0].key == templateName)
 					if (!existingTemplateDocumentSymbol) {
 						externalTemplates.push([baseFileTemplateDocumentSymbol, baseFileTextDocument.getText(baseFileTemplateDocumentSymbol.range)])
 					}
@@ -75,7 +77,7 @@ export function importPopfileTemplates(editor: TextEditor, edit: TextEditorEdit)
 	let insertPosition = ((): VDFPosition | null => {
 		for (let i = waveSchedule.length - 1; i >= 0; i--) {
 			const documentSymbol = waveSchedule[i]
-			if (documentSymbol.name.toLowerCase() == "templates" && documentSymbol.children != undefined) {
+			if (documentSymbol.key.toLowerCase() == "templates" && documentSymbol.children != undefined) {
 				return documentSymbol.range.end
 			}
 		}
@@ -92,10 +94,12 @@ export function importPopfileTemplates(editor: TextEditor, edit: TextEditorEdit)
 		insertPosition = ((): VDFPosition => {
 			const WaveMission = ["Wave", "Mission"].map(i => i.toLowerCase())
 			// Insert Templates before the first wave
-			const position = waveSchedule.find(i => WaveMission.includes(i.name.toLowerCase()))!.range.start // There must be at least 1 wave for templates to be referenced, otherwise we would have exited early
+			const position = waveSchedule.find(i => WaveMission.includes(i.key.toLowerCase()))!.range.start // There must be at least 1 wave for templates to be referenced, otherwise we would have exited early
 			return new VDFPosition(position.line, position.character)
 		})()
 	}
 
-	edit.insert(new Position(insertPosition.line, insertPosition.character - 1), text)
+	const edit = new WorkspaceEdit()
+	edit.insert(editor.document.uri, new Position(insertPosition.line, insertPosition.character - 1), text)
+	workspace.applyEdit(edit)
 }
