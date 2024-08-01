5dd60f95a735e83454162b12e8a3cd1c5fe34e21
cooolbros <pfwobcke@gmail.com>
2024-08-01 12:12:56 +1000

    Register importPopfileTemplates

--- apps/extension/desktop/client/src/extension.ts
+++ apps/extension/desktop/client/src/extension.ts
@@ -3,6 +3,7 @@ import { JSONToVDF } from "client/commands/JSONToVDF"
 import { VDFToJSON } from "client/commands/VDFToJSON"
 import { copyKeyValuePath } from "client/commands/copyKeyValuePath"
 import { extractVPKFileToWorkspace } from "client/commands/extractVPKFileToWorkspace"
+import { importPopfileTemplates } from "client/commands/importPopfileTemplates"
 import { showReferences } from "client/commands/showReferences"
 import { join } from "path"
 import { languageNames } from "utils/languageNames"
@@ -22,6 +23,7 @@ export function activate(context: ExtensionContext): void {
 
 	context.subscriptions.push(commands.registerTextEditorCommand("vscode-vdf.copyKeyValuePath", copyKeyValuePath))
 	context.subscriptions.push(commands.registerTextEditorCommand("vscode-vdf.extractVPKFileToWorkspace", extractVPKFileToWorkspace))
+	context.subscriptions.push(commands.registerTextEditorCommand("vscode-vdf.importPopfileTemplates", importPopfileTemplates))
 	context.subscriptions.push(commands.registerTextEditorCommand("vscode-vdf.JSONToVDF", JSONToVDF))
 	context.subscriptions.push(commands.registerTextEditorCommand("vscode-vdf.showReferences", showReferences))
 	context.subscriptions.push(commands.registerTextEditorCommand("vscode-vdf.VDFToJSON", VDFToJSON))
