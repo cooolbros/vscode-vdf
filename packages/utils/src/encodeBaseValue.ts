0a0ca8e39b269a1ef1313a06e90a76e081f456d7
cooolbros <pfwobcke@gmail.com>
2024-09-10 20:45:39 +1000

    Delete utils

--- packages/utils/src/encodeBaseValue.ts
+++
@@ -1,4 +0,0 @@
-export function encodeBaseValue(value: string): string {
-	// Don't call encodeURIComponent on #base value because '/' will get encoded
-	return value.split(/[/\\]+/).map(encodeURIComponent).join("/")
-}
--- packages/utils/src/normalizeUri.ts
+++
@@ -1,8 +0,0 @@
-export function normalizeUri(uri: string): string {
-	try {
-		return new URL(uri).href
-	}
-	catch (error) {
-		return uri
-	}
-}
