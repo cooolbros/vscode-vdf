107ae81556200ddf5097ee37c8daac28db3af25b
cooolbros <pfwobcke@gmail.com>
2024-10-29 00:09:17 +1100

    Simplify ArrayContainsArray

--- packages/utils/src/ArrayContainsArray.ts
+++ packages/utils/src/ArrayContainsArray.ts
@@ -1,45 +1,12 @@
-/**
- * Check to see if one array contains all items of another array in order
- * @param arr1 Outer array
- * @param arr2 Inner array
- * @returns `true` if the outer array contains all the elements of the inner array in order
- * @example ```
- * ArrayContainsArray(["a", "b", "c", "d"], ["b", "c"]) // true
- * ArrayContainsArray(["a", "b", "c", "d"], ["d", "e"]) // false
- * ArrayContainsArray(["a", "b", "c", "d"], []) // true
- * ```
- */
 export function ArrayContainsArray<T1, T2>(arr1: T1[], arr2: T2[], comparer: (a: T1, b: T2) => boolean): boolean {
 
 	if (arr2.length == 0) {
 		return true
 	}
 
-	// Impossible for arr1 to satisfy arr2 if it doesnt have enough length
 	if (arr1.length < arr2.length) {
 		return false
 	}
 
-	let i = 0
-	let index = 0
-
-	while (i < arr1.length) {
-		if (comparer(arr1[i], arr2[index])) {
-
-			while (index < arr2.length) {
-
-				if (!comparer(arr1[i], arr2[index])) {
-					return false
-				}
-
-				i++
-				index++
-			}
-
-			return true
-		}
-		i++
-	}
-
-	return false
+	return arr1.some((_, index) => arr2.every((v, i) => index + i < arr1.length && comparer(arr1[index + i], v)))
 }
