f2e9564c555df42a68e2593d77e3078a3cb3d787
cooolbros <pfwobcke@gmail.com>
2024-11-12 11:44:48 +1100

    Delete fileSystem router

--- packages/client/src/TRPCClientRouter.ts
+++ packages/client/src/TRPCClientRouter.ts
@@ -1,6 +1,6 @@
 import type { CombinedDataTransformer, initTRPC } from "@trpc/server"
 import { Uri } from "common/Uri"
-import { commands, FileType, languages, window, workspace } from "vscode"
+import { commands, languages, window, workspace } from "vscode"
 import { z } from "zod"
 import { searchForHUDRoot } from "./searchForHUDRoot"
 import type { FileSystemMountPoint } from "./VirtualFileSystem/FileSystemMountPoint"
@@ -21,78 +21,6 @@ export function TRPCClientRouter(
 ) {
 	const fileSystems = new Map<string, FileSystemMountPoint>()
 	return t.router({
-		fileSystem: t.router({
-			exists: t
-				.procedure
-				.input(URISchema)
-				.query(async ({ input }) => {
-					try {
-						await workspace.fs.stat(input.uri)
-						return true
-					}
-					catch (error: any) {
-						return false
-					}
-				}),
-			stat: t
-				.procedure
-				.input(URISchema)
-				.query(async ({ input }) => {
-					return workspace.fs.stat(input.uri)
-				}),
-			readFile: t
-				.procedure
-				.input(URISchema)
-				.query(async ({ input }) => {
-					const arr = await workspace.fs.readFile(input.uri)
-					if (arr[0] == 255 && arr[1] == 254) {
-						return UTF16LEDecoder.decode(arr)
-					}
-					return UTF8Decoder.decode(arr)
-				}),
-			readFileBinary: t
-				.procedure
-				.input(
-					URISchema
-						.merge(
-							z.object({
-								begin: z.number().optional(),
-								end: z.number().optional()
-							})
-						)
-				)
-				.query(async ({ input }) => {
-					return new Uint8Array((await workspace.fs.readFile(input.uri)).subarray(input.begin, input.end))
-				}),
-			readDirectory: t
-				.procedure
-				.input(
-					URISchema
-						.merge(
-							z.object({
-								recursive: z.boolean().default(false)
-							})
-						)
-				)
-				.query(async ({ input }) => {
-					if (!input.recursive) {
-						return await workspace.fs.readDirectory(input.uri)
-					}
-
-					const paths: [string, FileType][] = []
-					const iterateDirectory = async (relativePath: string) => {
-						for (const [name, type] of await workspace.fs.readDirectory(input.uri.with({ path: `${input.uri.path}/${relativePath}` }))) {
-							const path = relativePath != "" ? `${relativePath}/${name}` : name
-							paths.push([path, type])
-							if (type == FileType.Directory) {
-								await iterateDirectory(path)
-							}
-						}
-					}
-					await iterateDirectory("")
-					return paths
-				})
-		}),
 		searchForHUDRoot: t
 			.procedure
 			.input(URISchema)
