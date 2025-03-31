b786d95c5e27237513f6bde4a6ca299816631642
cooolbros <pfwobcke@gmail.com>
2025-03-31 14:47:39 +1100

    Update API
    Add HEAD request handler
    Add public Cache-Control of 30 days
    Add SqliteConnection service
    Use journal_mode=DELETE

--- apps/API/Program.cs
+++ apps/API/Program.cs
@@ -4,13 +4,27 @@ using System.Data;
 using System.IO;
 using System.IO.Compression;
 using System.Net;
+using System.Net.Http.Headers;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Data.Sqlite;
+using Microsoft.Extensions.DependencyInjection;
 
 WebApplicationBuilder builder = WebApplication.CreateSlimBuilder(args);
+builder.Services.AddScoped((sp) =>
+{
+	SqliteConnection connection = new("Data Source=tf.db;Mode=ReadOnly");
+	connection.Open();
+
+	using SqliteCommand command = connection.CreateCommand();
+	command.CommandText = "PRAGMA journal_mode=DELETE;";
+	command.ExecuteNonQuery();
+
+	return connection;
+});
+
 WebApplication app = builder.Build();
 
 JsonSerializerOptions options = new()
@@ -19,20 +33,60 @@ JsonSerializerOptions options = new()
 	TypeInfoResolver = AppJsonSerializerContext.Default,
 };
 
-app.MapMethods("{**path}", ["GET", "HEAD"], (HttpRequest request, HttpResponse response, string path = "") =>
+app.Use((context, next) =>
 {
-	SqliteConnection connection = new("Data Source=tf.db;Mode=ReadOnly");
-	connection.Open();
+	context.Response.Headers.CacheControl = new CacheControlHeaderValue
+	{
+		Public = true,
+		MaxAge = TimeSpan.FromDays(30)
+	}.ToString();
 
-	bool get = request.Method == "GET";
+	return next();
+});
 
+app.MapMethods("{**path}", ["HEAD"], (HttpRequest request, HttpResponse response, SqliteConnection connection, string path = "") =>
+{
+	path = Path.TrimEndingDirectorySeparator(path);
+
+	using SqliteCommand command = connection.CreateCommand();
+	command.CommandText = $"""
+		SELECT
+			CASE WHEN "name" = $path THEN "bytes" ELSE 0 END AS "bytes"
+		FROM "tf"
+		WHERE "tf"."name" = $path OR "tf"."name" LIKE $path || '/%'
+		LIMIT 1;
+	""";
+	command.Parameters.AddWithValue("$path", path);
+
+	using SqliteDataReader reader = command.ExecuteReader(CommandBehavior.SingleRow);
+	if (!reader.Read())
+	{
+		return Results.NotFound();
+	}
+
+	response.Headers.ContentLength = reader.GetInt32("bytes");
+	response.Headers.ContentType = reader.GetInt32("bytes") switch
+	{
+		0 => "application/json",
+		_ => "application/octet-stream"
+	};
+
+	return Results.Empty;
+});
+
+app.MapGet("{**path}", (HttpRequest request, HttpResponse response, SqliteConnection connection, string path = "") =>
+{
+	path = Path.TrimEndingDirectorySeparator(path);
 	switch (request.Headers.Accept.ToString())
 	{
 		case "application/octet-stream":
 			{
 				using SqliteCommand command = connection.CreateCommand();
 				command.CommandText = $"""
-					SELECT "bytes"{(get ? ", \"data\"" : "")} FROM "tf"
+					SELECT
+						"bytes",
+						"data"
+					FROM "tf"
 					WHERE "name" = $path
 					LIMIT 1;
 				""";
@@ -45,10 +99,7 @@ app.MapMethods("{**path}", ["GET", "HEAD"], (HttpRequest request, HttpResponse r
 				}
 
 				response.Headers.ContentLength = reader.GetInt32("bytes");
-
-				return get
-					? Results.Stream(new BrotliStream(reader.GetStream("data"), CompressionMode.Decompress))
-					: Results.Empty;
+				return Results.Stream(new BrotliStream(reader.GetStream("data"), CompressionMode.Decompress));
 			}
 		case "application/json":
 			{
@@ -61,8 +112,6 @@ app.MapMethods("{**path}", ["GET", "HEAD"], (HttpRequest request, HttpResponse r
 					FROM (SELECT SUBSTRING("name", $length + 2) AS "name" FROM "tf" WHERE "tf"."name" LIKE $path || '/%')
 					ORDER BY "type" DESC, "name" ASC;
 				""";
-
-				path = Path.TrimEndingDirectorySeparator(path);
 
 				command.Parameters.AddWithValue("$path", path);
 				command.Parameters.AddWithValue("$length", path.Length);
