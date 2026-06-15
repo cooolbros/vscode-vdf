f61e697e832dc5f550c45ccbb6271569a6f94e40
cooolbros <cooolbros@users.noreply.github.com>
2026-06-15 13:12:47 +0000

    deploy: 22e78c330a9c31e4c968e2d584167bbb47efeaf4

---
+++ _next/static/chunks/pages/_app-513689c00b1f5a0a.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[636],{4142:(_,n,p)=>{(window.__NEXT_P=window.__NEXT_P||[]).push(["/_app",function(){return p(8313)}])}},_=>{var n=n=>_(_.s=n);_.O(0,[593,792],()=>(n(4142),n(8701))),_N_E=_.O()}]);
