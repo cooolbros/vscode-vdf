6f2bf2e3a104b98ccd26cf1f44ea1cc480be5f89
cooolbros <pfwobcke@gmail.com>
2026-07-09 11:45:16 +1000

    Add dispose$

--- packages/server/src/WorkspaceBase.ts
+++ packages/server/src/WorkspaceBase.ts
@@ -1,14 +1,21 @@
 import type { Uri } from "common/Uri"
+import { ReplaySubject } from "rxjs"
 
-export abstract class WorkspaceBase {
+export abstract class WorkspaceBase implements AsyncDisposable {
 
 	public readonly uri: Uri
+	public readonly dispose$: ReplaySubject<void>
 
 	constructor(uri: Uri) {
 		this.uri = uri
+		this.dispose$ = new ReplaySubject(1)
 	}
 
 	public relative(uri: Uri) {
 		return this.uri.relative(uri)
+	}
+
+	public async [Symbol.asyncDispose](): Promise<void> {
+		this.dispose$.next()
 	}
 }
